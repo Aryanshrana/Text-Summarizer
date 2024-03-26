efebe6e0915926f0b59b285a3d28661ee6e16655
Amnnnn16 <ag6835902@gmail.com>
2024-03-26 15:27:56 +0530

    final

--- summary.py
+++ summary.py
@@ -3,7 +3,7 @@ from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 from heapq import nlargest
 
-text=''' In 1980, Samsung acquired the Gumi-based Hanguk Jeonja Tongsin and entered telecommunications hardware. Its early products were switchboards. The facility was developed into the telephone and fax manufacturing systems and became the center of Samsung's mobile phone manufacturing. They have produced over 800 million mobile phones to date.[21] The company grouped them together under Samsung Electronics in the 1980s. '''
+#text=''' In 1980, Samsung acquired the Gumi-based Hanguk Jeonja Tongsin and entered telecommunications hardware. Its early products were switchboards. The facility was developed into the telephone and fax manufacturing systems and became the center of Samsung's mobile phone manufacturing. They have produced over 800 million mobile phones to date.[21] The company grouped them together under Samsung Electronics in the 1980s. '''
 
 def summarizer(raw):
     stopwords=list(STOP_WORDS)
